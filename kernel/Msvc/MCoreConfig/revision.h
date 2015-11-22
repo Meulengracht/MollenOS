b4c0aa18d08c05c7110234140e79baf4b38e66bb
Fadekraft <meulengracht@andromeda-it.dk>
2015-11-22 13:00:49 +0100

    Merged back into master
    Driver rewrite was a success, returning to master branch

--- kernel/Msvc/MCoreConfig/revision.h
+++ kernel/Msvc/MCoreConfig/revision.h
@@ -7,11 +7,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "22 November 2015"
-#define BUILD_TIME "12:50:15"
+#define BUILD_TIME "13:00:19"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 2
-#define REVISION_BUILD 2232
+#define REVISION_BUILD 2233
 
 #endif
 
