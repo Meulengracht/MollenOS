4369206c8eaaaeae38e2dbb19c581ff2a6af0bed
Fadekraft <meulengracht@andromeda-it.dk>
2015-12-18 12:26:19 +0100

    A bit more rendering initialization

--- userspace/System/MWinMgr/MWinMgr.c
+++ userspace/System/MWinMgr/MWinMgr.c
@@ -26,16 +26,39 @@
 /* Entry Point */
 int main(int argc, char* argv[])
 {
+	/* Variables */
+	SDL_Window *MainWnd;
+	SDL_Renderer *MainRenderer;
+
 	/* Init SDL (Main) */
 	SDL_SetMainReady();
 
 	/* Init SDL (Full) */
-	if (SDL_Init(SDL_INIT_VIDEO) != 0){
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		MollenOSSystemLog(SDL_GetError());
 		return -1;
 	}
 
+	/* Create a window */
+	MainWnd = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (MainWnd == NULL) {
+		MollenOSSystemLog(SDL_GetError());
+		SDL_Quit();
+		return -1;
+	}
+
+	/* Create renderer */
+	MainRenderer = SDL_CreateRenderer(MainWnd, -1, SDL_RENDERER_SOFTWARE);
+	if (MainRenderer == NULL) {
+		MollenOSSystemLog(SDL_GetError());
+		SDL_DestroyWindow(MainWnd);
+		SDL_Quit();
+		return -1;
+	}
+
 	/* Cleanup */
+	SDL_DestroyRenderer(MainRenderer);
+	SDL_DestroyWindow(MainWnd);
 	SDL_Quit();
 
 	/* Done! */
