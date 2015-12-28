2020e0a1761f961b1f71a1d37d90ffe4ab1c2ab2
Fadekraft <meulengracht@andromeda-it.dk>
2015-12-28 18:31:06 +0100

    Logging seems to work
    The new logging system seems to work without issues, that's ..
    disturbing

--- kernel/Msvc/MCoreConfig/revision.h
+++ kernel/Msvc/MCoreConfig/revision.h
@@ -7,11 +7,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "28 December 2015"
-#define BUILD_TIME "18:05:32"
+#define BUILD_TIME "18:18:15"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 2
-#define REVISION_BUILD 2490
+#define REVISION_BUILD 2491
 
 #endif
 
