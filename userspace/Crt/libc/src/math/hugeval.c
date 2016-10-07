911d55ff145579b989a2751fe96384b8fad76a12
Fadekraft <meulengracht@andromeda-it.dk>
2016-10-07 14:39:11 +0200

    Cleaned up some old stuff

--- userspace/Crt/libc/src/math/hugeval.c
+++
@@ -1,4 +0,0 @@
-#include <internal/_all.h>
-
-#undef _HUGE
-double_s _HUGE = { 0x00000, 0x00000, 0x7ff, 0x0 };
--- userspace/Crt/libc/src/string/atol.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-
-/*
- * @implemented
- */
-long atol(const char *str)
-{
-    return (long)atoi64(str);
-}
