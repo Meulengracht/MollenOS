cb63b6a8823c7e0cb7ec5501af031746cd84aa9b
Fadekraft <meulengracht@andromeda-it.dk>
2016-12-19 10:25:28 +0100

    #65 #66 Added UML for the new driver/device arch design

--- kernel/Msvc/MCoreConfig/revision.h
+++ kernel/Msvc/MCoreConfig/revision.h
@@ -6,12 +6,12 @@
 #ifndef _REVISION_H_
 #define _REVISION_H_
 
-#define BUILD_DATE "16 December 2016"
-#define BUILD_TIME "14:27:49"
+#define BUILD_DATE "19 December 2016"
+#define BUILD_TIME "10:24:00"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 3
-#define REVISION_BUILD 3972
+#define REVISION_BUILD 3973
 
 #endif
 
