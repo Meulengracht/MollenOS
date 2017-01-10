6dbe38325fb5aed2ceefbe6aaba4709034a624f8
Fadekraft <meulengracht@andromeda-it.dk>
2017-01-10 10:56:18 +0100

    Wubs

--- kernel/msvc/MCoreConfig/revision.h
+++ kernel/msvc/MCoreConfig/revision.h
@@ -7,9 +7,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "10 January 2017"
+#define BUILD_TIME "10:53:29"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
+#define REVISION_BUILD 4040
 
 #endif
 
