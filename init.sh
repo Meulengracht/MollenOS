7ff8326e1365e214fd9c7a3b5789f2c04c49b6d0
Philip Meulengracht <pmeu@oticon.com>
2017-05-23 09:40:34 +0200

    improvements to init script

--- init.sh
+++ init.sh
@@ -2,14 +2,16 @@
 
 if [ ! -f /usr/local/cross/bin/clang ]; then
   clear
-  mkdir toolchain
+  mkdir -p toolchain
   cd toolchain
   
   if ! [ -x "$(command -v svn)" ]; then
     apt-get install subversion
   fi
   
-  if ! [ -x "$(command -v cmake)" ]; then
+  CMAKE_VERSION="$(cmake --version)"
+  echo ${CMAKE_VERSION}
+  if ! [[ "$CMAKE_VERSION" =~ "cmake version 3" ]]; then
     wget https://cmake.org/files/v3.8/cmake-3.8.1.tar.gz
     tar xvzf cmake-3.8.1.tar.gz
     rm cmake-3.8.1.tar.gz
@@ -20,23 +22,25 @@ if [ ! -f /usr/local/cross/bin/clang ]; then
   fi
   
   if ! [ -x "$(command -v clang)" ]; then
-    svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
-    cd llvm/tools
-    svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
-    cd ../..
+    if [ ! -f ./llvm ]; then
+	  svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+      cd llvm/tools
+      svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+      cd ../..
+      
+      cd llvm/tools/clang/tools
+      svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+      cd ../../../..
+      
+      cd llvm/projects
+      svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+      svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
+      svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
+      svn co http://llvm.org/svn/llvm-project/openmp/trunk openmp
+      cd ../..
+	fi
     
-    cd llvm/tools/clang/tools
-    svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
-    cd ../../../..
-    
-    cd llvm/projects
-    svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
-    svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
-    svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
-    svn co http://llvm.org/svn/llvm-project/openmp/trunk openmp
-    cd ../..
-
-	mkdir build
+	mkdir -p build
 	cd build
     cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_EH=True -DLLVM_ENABLE_RTTI=True  ../llvm
     make 
@@ -44,8 +48,8 @@ if [ ! -f /usr/local/cross/bin/clang ]; then
 	cd ..
   fi
 
-  mkdir /usr/local/cross
-  mkdir cross-build
+  mkdir -p /usr/local/cross
+  mkdir -p cross-build
   cd cross-build
   cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_RUNTIME=Off -DLLVM_INCLUDE_TESTS=Off -DLLVM_INCLUDE_EXAMPLES=Off -DLLVM_ENABLE_BACKTRACES=Off -DCMAKE_CROSSCOMPILING=True -DCMAKE_INSTALL_PREFIX=/usr/local/cross -DLLVM_TABLEGEN=llvm-tblgen -DCLANG_TABLEGEN=clang-tblgen -DLLVM_DEFAULT_TARGET_TRIPLE=i386-pc-win32 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_USE_LINKER=lld ../llvm
   make 
