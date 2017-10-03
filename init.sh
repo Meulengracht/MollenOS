35f5c986db79751b90476cbc5db4147df4303945
Philip Meulengracht <phailmollen@gmail.com>
2017-10-03 12:19:53 +0200

    Changed the default cross directory

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -f /usr/local/cross/bin/clang ]; then
+if [ ! -f $CROSS/bin/clang ]; then
   cd toolchain
   
   if ! [ -x "$(command -v clang)" ]; then
@@ -31,7 +31,7 @@ if [ ! -f /usr/local/cross/bin/clang ]; then
 	cd ..
   fi
 
-  mkdir -p /usr/local/cross
+  mkdir -p $CROSS
   mkdir -p cross-build
   cd cross-build
   cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_RUNTIME=Off -DLLVM_INCLUDE_TESTS=Off -DLLVM_INCLUDE_EXAMPLES=Off -DLLVM_ENABLE_BACKTRACES=Off -DCMAKE_CROSSCOMPILING=True -DCMAKE_INSTALL_PREFIX=/usr/local/cross -DLLVM_TABLEGEN=llvm-tblgen -DCLANG_TABLEGEN=clang-tblgen -DLLVM_DEFAULT_TARGET_TRIPLE=i386-pc-win32 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_USE_LINKER=lld ../llvm
