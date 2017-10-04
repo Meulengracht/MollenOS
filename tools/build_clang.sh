7fdeaf0cfa6562fc607845e66ac1bbcf9aebfcf3
Philip Meulengracht <phailmollen@gmail.com>
2017-10-04 12:06:17 +0200

    Now we require clang on travis

--- tools/build_clang.sh
+++ tools/build_clang.sh
@@ -4,6 +4,7 @@ if [ ! -f $CROSS/bin/clang ]; then
   cd ../toolchain
   
   if ! [ -x "$(command -v clang)" ]; then
+	mkdir -p build
 	cd build
     cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_EH=True -DLLVM_ENABLE_RTTI=True  ../llvm
     make 
--- tools/checkout.sh
+++ tools/checkout.sh
@@ -3,27 +3,23 @@
 if [ ! -f $CROSS/bin/clang ]; then
   cd ../toolchain
   
-  if ! [ -x "$(command -v clang)" ]; then
-    if [ ! -f ./llvm ]; then
-	  svn -q co http://llvm.org/svn/llvm-project/llvm/trunk llvm
-      cd llvm/tools
-      svn -q co http://llvm.org/svn/llvm-project/cfe/trunk clang
-      cd ../..
-      
-      cd llvm/tools/clang/tools
-      svn -q co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
-      cd ../../../..
-      
-      cd llvm/projects
-      svn -q co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
-      svn -q co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
-      svn -q co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
-      svn -q co http://llvm.org/svn/llvm-project/openmp/trunk openmp
-      svn -q co http://llvm.org/svn/llvm-project/lld/trunk lld
-      cd ../..
-	fi
+  if [ ! -f ./llvm ]; then
+    svn -q co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+    cd llvm/tools
+    svn -q co http://llvm.org/svn/llvm-project/cfe/trunk clang
+    cd ../..
     
-	mkdir -p build
+    cd llvm/tools/clang/tools
+    svn -q co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+    cd ../../../..
+    
+    cd llvm/projects
+    svn -q co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+    svn -q co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
+    svn -q co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
+    svn -q co http://llvm.org/svn/llvm-project/openmp/trunk openmp
+    svn -q co http://llvm.org/svn/llvm-project/lld/trunk lld
+    cd ../..
   fi
 
   mkdir -p $CROSS
