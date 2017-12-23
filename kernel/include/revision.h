afd349d0ee254917f82592824a7bb0e240df5665
Philip Meulengracht <phailmollen@gmail.com>
2017-12-23 14:04:01 +0100

    Trigger teamcity

--- kernel/include/revision.h
+++ kernel/include/revision.h
@@ -3,12 +3,12 @@
 #ifndef _REVISION_H_
 #define _REVISION_H_
 
-#define BUILD_DATE "20 December 2017"
-#define BUILD_TIME "15:24:52"
+#define BUILD_DATE "23 December 2017"
+#define BUILD_TIME "14:03:47"
 #define BUILD_SYSTEM "clang"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 6504
+#define REVISION_BUILD 6505
 
 #endif //!_REVISION_H_
