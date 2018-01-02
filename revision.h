c5094bb844d1e49a89f4eb9bb1cf449997856a70
Philip Meulengracht <phailmollen@gmail.com>
2018-01-02 15:22:48 +0100

    Cleaned up rest of unused files

--- revision.h
+++ revision.h
@@ -4,11 +4,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "02 January 2018"
-#define BUILD_TIME "14:35:03"
+#define BUILD_TIME "15:17:51"
 #define BUILD_SYSTEM "clang"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 6727
+#define REVISION_BUILD 6728
 
 #endif //!_REVISION_H_
