b1baa7148521e337ad44b618af014aeef44acd1c
Philip Meulengracht <phailmollen@gmail.com>
2018-01-29 15:38:57 +0100

    Ported most of mesa, still missing the last gallium driver

--- kernel/include/revision.h
+++ kernel/include/revision.h
@@ -3,12 +3,12 @@
 #ifndef _REVISION_H_
 #define _REVISION_H_
 
-#define BUILD_DATE "28 January 2018"
-#define BUILD_TIME "21:49:10"
+#define BUILD_DATE "29 January 2018"
+#define BUILD_TIME "09:12:45"
 #define BUILD_SYSTEM "clang"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 6887
+#define REVISION_BUILD 6889
 
 #endif //!_REVISION_H_
