2f4b1eb7fbcad64dcd9f31bf6cee87e23584c69c
Philip Meulengracht <phailmollen@gmail.com>
2018-02-06 14:32:26 +0100

    fixed an issue with the defines in new threading header

--- revision.h
+++ revision.h
@@ -4,11 +4,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "06 February 2018"
-#define BUILD_TIME "13:43:33"
+#define BUILD_TIME "14:26:29"
 #define BUILD_SYSTEM "clang"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 7034
+#define REVISION_BUILD 7036
 
 #endif //!_REVISION_H_
