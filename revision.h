9ceb9de41f5fd80a2198b931cca8c77038eb07cb
Philip <phailmollen@gmail.com>
2018-07-18 15:17:17 +0200

    Found yet another issue with mesa - this time another llvm intrinsinc that had been removed, so had to compensate for that, but now it seems everything is building

--- revision.h
+++ revision.h
@@ -4,11 +4,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "18 July 2018"
-#define BUILD_TIME "13:01:06"
+#define BUILD_TIME "14:33:58"
 #define BUILD_SYSTEM "clang"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 8445
+#define REVISION_BUILD 8447
 
 #endif //!_REVISION_H_
