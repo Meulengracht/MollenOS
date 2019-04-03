caf6fd4bce2ea79d57f52a37936815536c9f2a8b
Philip Meulengracht <phailmollen@gmail.com>
2019-04-03 07:32:52 +0000

    Fixed def for libm and libc

--- kernel/include/revision.h
+++ kernel/include/revision.h
@@ -3,12 +3,12 @@
 #ifndef _REVISION_H_
 #define _REVISION_H_
 
-#define BUILD_DATE "02 April 2019"
-#define BUILD_TIME "12:21:06"
+#define BUILD_DATE "03 April 2019"
+#define BUILD_TIME "07:30:26"
 #define BUILD_SYSTEM "clang"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 10750
+#define REVISION_BUILD 10752
 
 #endif //!_REVISION_H_
