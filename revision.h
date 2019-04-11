177881b5feb0c72d5ffb60778a8a2eebccb0e5a4
Philip Meulengracht <phailmollen@gmail.com>
2019-04-11 06:13:03 +0000

    Updated compiler-rt

--- revision.h
+++ revision.h
@@ -4,11 +4,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "11 April 2019"
-#define BUILD_TIME "05:45:00"
+#define BUILD_TIME "06:10:49"
 #define BUILD_SYSTEM "clang"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 10781
+#define REVISION_BUILD 10782
 
 #endif //!_REVISION_H_
