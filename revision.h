97ee6f95d29d19bf927bb0e1d4502a13db2cd94c
Philip Meulengracht <phailmollen@gmail.com>
2019-06-14 11:13:05 +0000

    Added skeleton code for netmanager

--- revision.h
+++ revision.h
@@ -4,11 +4,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "14 June 2019"
-#define BUILD_TIME "10:45:09"
+#define BUILD_TIME "10:59:58"
 #define BUILD_SYSTEM "clang"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 6
-#define REVISION_BUILD 11497
+#define REVISION_BUILD 11499
 
 #endif //!_REVISION_H_
---
+++ services/netmanager/main.c
@@ -0,0 +1,63 @@
+/* MollenOS
+ *
+ * Copyright 2018, Philip Meulengracht
+ *
+ * This program is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation ? , either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Network Manager
+ * - Contains the implementation of the network-manager which keeps track
+ *   of all users and their running applications.
+ */
+#define __TRACE
+
+#include <ds/collection.h>
+#include <os/services/process.h>
+#include <ddk/services/session.h>
+#include <ddk/service.h>
+#include <ddk/utils.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static UUId_t ServiceHandleGenerator = 0;
+
+OsStatus_t
+OnLoad(void)
+{
+	return RegisterService(__NETMANAGER_TARGET);
+}
+
+OsStatus_t
+OnUnload(void)
+{
+    return OsSuccess;
+}
+
+OsStatus_t
+OnEvent(
+	_In_ MRemoteCall_t* Message)
+{
+    OsStatus_t Result = OsSuccess;
+    
+    TRACE("Networkmanager.OnEvent(%i)", Message->Function);
+
+    switch (Message->Function) {
+        
+        default: {
+            break;
+        }
+    }
+    return Result;
+}
