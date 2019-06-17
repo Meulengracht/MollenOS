797323d25d1d6dfdd99a9642f8febd5bdb0eae58
Philip Meulengracht <phailmollen@gmail.com>
2019-06-17 13:40:11 +0000

    infrastructure code to support different kinds of net addresses

---
+++ librt/libc/include/inet/internet.h
@@ -0,0 +1,97 @@
+
+#ifndef __INET_INTERNET_H__
+#define __INET_INTERNET_H__
+
+#include <inet/socket.h>
+
+#ifndef	_IN_PORT_T_DEFINED_
+#define	_IN_PORT_T_DEFINED_
+typedef	uint16_t in_port_t;
+#endif
+
+#ifndef	_IN_ADDR_T_DEFINED_
+#define	_IN_ADDR_T_DEFINED_
+typedef	uint32_t in_addr_t;
+#endif
+
+// Definitions of the bits in an Internet address integer.
+// On subnets, host and network parts are found according to
+// the subnet mask, not these masks.
+#define IN_CLASSA(a)       ((((in_addr_t)(a)) & 0x80000000) == 0)
+#define IN_CLASSA_NET      0xff000000
+#define IN_CLASSA_NSHIFT   24
+#define IN_CLASSA_HOST     (0xffffffff & ~IN_CLASSA_NET)
+#define IN_CLASSA_MAX      128
+#define IN_CLASSB(a)       ((((in_addr_t)(a)) & 0xc0000000) == 0x80000000)
+#define IN_CLASSB_NET      0xffff0000
+#define IN_CLASSB_NSHIFT   16
+#define IN_CLASSB_HOST     (0xffffffff & ~IN_CLASSB_NET)
+#define IN_CLASSB_MAX      65536
+#define IN_CLASSC(a)       ((((in_addr_t)(a)) & 0xe0000000) == 0xc0000000)
+#define IN_CLASSC_NET      0xffffff00
+#define IN_CLASSC_NSHIFT   8
+#define IN_CLASSC_HOST     (0xffffffff & ~IN_CLASSC_NET)
+#define IN_CLASSD(a)       ((((in_addr_t)(a)) & 0xf0000000) == 0xe0000000)
+#define IN_MULTICAST(a)    IN_CLASSD(a)
+#define IN_EXPERIMENTAL(a) ((((in_addr_t)(a)) & 0xe0000000) == 0xe0000000)
+#define IN_BADCLASS(a)     ((((in_addr_t)(a)) & 0xf0000000) == 0xf0000000)
+
+/* Address to accept any incoming messages.  */
+#define INADDR_ANY ((in_addr_t) 0x00000000)
+
+/* Address to send to all hosts.  */
+#define INADDR_BROADCAST ((in_addr_t) 0xffffffff)
+
+/* Address indicating an error return.  */
+#define INADDR_NONE ((in_addr_t) 0xffffffff)
+
+/* Network number for local host loopback.  */
+#define IN_LOOPBACKNET 127
+
+/* Address to loopback in software to local host.  */
+#ifndef INADDR_LOOPBACK
+#define INADDR_LOOPBACK ((in_addr_t) 0x7f000001) /* Inet 127.0.0.1.  */
+#endif
+
+/* Defines for Multicast INADDR.  */
+#define INADDR_UNSPEC_GROUP    ((in_addr_t) 0xe0000000) /* 224.0.0.0 */
+#define INADDR_ALLHOSTS_GROUP  ((in_addr_t) 0xe0000001) /* 224.0.0.1 */
+#define INADDR_ALLRTRS_GROUP   ((in_addr_t) 0xe0000002) /* 224.0.0.2 */
+#define INADDR_MAX_LOCAL_GROUP ((in_addr_t) 0xe00000ff) /* 224.0.0.255 */
+
+// ip4 address
+struct in_addr {
+    in_addr_t s_addr; 
+}
+
+// ip6 address
+struct in6_addr
+{
+    union {
+        uint8_t  __u6_addr8[16];
+        uint16_t __u6_addr16[8];
+        uint32_t __u6_addr32[4];
+    } __in6_u;
+#define s6_addr    __in6_u.__u6_addr8
+#define s6_addr16  __in6_u.__u6_addr16
+#define s6_addr32  __in6_u.__u6_addr32
+};
+
+struct sockaddr_in {
+	uint8_t          sin_len;
+	sa_family_t      sin_family;   // e.g. AF_INET, AF_INET6
+    unsigned short   sin_port;     // e.g. htons(3490)
+    struct in_addr   sin_addr;     // see struct in_addr, below
+    char             sin_zero[8];  // zero this if you want to
+};
+
+struct sockaddr_in6 {
+	uint8_t         sin6_len;
+    uint16_t        sin6_family;   // address family, AF_INET6
+    uint16_t        sin6_port;     // port number, Network Byte Order
+    uint32_t        sin6_flowinfo; // IPv6 flow information
+    struct in6_addr sin6_addr;     // IPv6 address
+    uint32_t        sin6_scope_id; // Scope ID
+};
+
+#endif //!__INET_INTERNET_H__
--- librt/libc/include/inet/socket.h
+++ librt/libc/include/inet/socket.h
@@ -28,12 +28,12 @@
 
 #ifndef	_SOCKLEN_T_DEFINED_
 #define	_SOCKLEN_T_DEFINED_
-typedef	__socklen_t	socklen_t;	/* length type for network syscalls */
+typedef	__int_least32_t	socklen_t;
 #endif
 
 #ifndef	_SA_FAMILY_T_DEFINED_
 #define	_SA_FAMILY_T_DEFINED_
-typedef	__sa_family_t sa_family_t;	/* sockaddr address family type */
+typedef	uint16_t sa_family_t;
 #endif
 
 // Socket types
@@ -82,47 +82,13 @@ struct	linger {
 #define	SOL_SOCKET	0xffff
 
 //Address families.
-#define	AF_UNSPEC	0		/* unspecified */
-#define	AF_UNIX		1		/* local to host */
-#define	AF_LOCAL	AF_UNIX		/* draft POSIX compatibility */
-#define	AF_INET		2		/* internetwork: UDP, TCP, etc. */
-#define	AF_IMPLINK	3		/* arpanet imp addresses */
-#define	AF_PUP		4		/* pup protocols: e.g. BSP */
-#define	AF_CHAOS	5		/* mit CHAOS protocols */
-#define	AF_NS		6		/* XEROX NS protocols */
-#define	AF_ISO		7		/* ISO protocols */
-#define	AF_OSI		AF_ISO
-#define	AF_ECMA		8		/* european computer manufacturers */
-#define	AF_DATAKIT	9		/* datakit protocols */
-#define	AF_CCITT	10		/* CCITT protocols, X.25 etc */
-#define	AF_SNA		11		/* IBM SNA */
-#define AF_DECnet	12		/* DECnet */
-#define AF_DLI		13		/* DEC Direct data link interface */
-#define AF_LAT		14		/* LAT */
-#define	AF_HYLINK	15		/* NSC Hyperchannel */
-#define	AF_APPLETALK	16		/* Apple Talk */
-#define	AF_ROUTE	17		/* Internal Routing Protocol */
-#define	AF_LINK		18		/* Link layer interface */
-#define	pseudo_AF_XTP	19		/* eXpress Transfer Protocol (no AF) */
-#define	AF_COIP		20		/* connection-oriented IP, aka ST II */
-#define	AF_CNT		21		/* Computer Network Technology */
-#define pseudo_AF_RTIP	22		/* Help Identify RTIP packets */
-#define	AF_IPX		23		/* Novell Internet Protocol */
-#define	AF_INET6	24		/* IPv6 */
-#define pseudo_AF_PIP	25		/* Help Identify PIP packets */
-#define AF_ISDN		26		/* Integrated Services Digital Network*/
-#define AF_E164		AF_ISDN		/* CCITT E.164 recommendation */
-#define AF_NATM		27		/* native ATM access */
-#define	AF_ENCAP	28
-#define	AF_SIP		29		/* Simple Internet Protocol */
-#define AF_KEY		30
-#define pseudo_AF_HDRCMPLT 31		/* Used by BPF to not rewrite headers
-					   in interface output routine */
-#define	AF_BLUETOOTH	32		/* Bluetooth */
-#define AF_MPLS         33              /* MPLS */
-#define pseudo_AF_PFLOW 34		/* pflow */
-#define pseudo_AF_PIPEX 35		/* PIPEX */
-#define AF_MAX          36
+#define	AF_UNSPEC		0		/* unspecified */
+#define	AF_UNIX			1		/* local to host */
+#define	AF_LOCAL		AF_UNIX /* only to keep unix compatability */
+#define	AF_INET			2		/* internetwork: UDP, TCP, etc. */
+#define	AF_INET6		3		/* IPv6 */
+#define	AF_BLUETOOTH	4		/* Bluetooth */
+#define AF_MAX          5
 
 // Structure used to store most addresses.
 struct sockaddr {
@@ -149,40 +115,8 @@ struct sockaddr_storage {
 #define	PF_LOCAL	    AF_LOCAL
 #define	PF_UNIX		    AF_UNIX
 #define	PF_INET		    AF_INET
-#define	PF_IMPLINK	    AF_IMPLINK
-#define	PF_PUP		    AF_PUP
-#define	PF_CHAOS	    AF_CHAOS
-#define	PF_NS		    AF_NS
-#define	PF_ISO		    AF_ISO
-#define	PF_OSI		    AF_ISO
-#define	PF_ECMA		    AF_ECMA
-#define	PF_DATAKIT	    AF_DATAKIT
-#define	PF_CCITT	    AF_CCITT
-#define	PF_SNA		    AF_SNA
-#define PF_DECnet	    AF_DECnet
-#define PF_DLI		    AF_DLI
-#define PF_LAT		    AF_LAT
-#define	PF_HYLINK	    AF_HYLINK
-#define	PF_APPLETALK	AF_APPLETALK
-#define	PF_ROUTE	    AF_ROUTE
-#define	PF_LINK		    AF_LINK
-#define	PF_XTP		    pseudo_AF_XTP	/* really just proto family, no AF */
-#define	PF_COIP		    AF_COIP
-#define	PF_CNT		    AF_CNT
-#define	PF_IPX		    AF_IPX		/* same format as AF_NS */
 #define PF_INET6	    AF_INET6
-#define PF_RTIP		    pseudo_AF_RTIP	/* same format as AF_INET */
-#define PF_PIP		    pseudo_AF_PIP
-#define PF_ISDN		    AF_ISDN
-#define PF_NATM		    AF_NATM
-#define PF_ENCAP	    AF_ENCAP
-#define	PF_SIP		    AF_SIP
-#define PF_KEY		    AF_KEY
-#define PF_BPF		    pseudo_AF_HDRCMPLT
 #define	PF_BLUETOOTH	AF_BLUETOOTH
-#define PF_MPLS		    AF_MPLS
-#define PF_PFLOW	    pseudo_AF_PFLOW
-#define PF_PIPEX	    pseudo_AF_PIPEX
 #define	PF_MAX		    AF_MAX
 
 // These are the valid values for the "how" field used by shutdown(2).
