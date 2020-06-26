5b4f332fb0f22274fbf7ca1052135ad7bd336634
Philip Meulengracht <phailmollen@gmail.com>
2020-06-26 09:46:10 +0200

    Update depends.sh

--- tools/depends.sh
+++ tools/depends.sh
@@ -4,7 +4,7 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 # Dev-libraries
 apt-get update
-apt-get -y -qq git cmake gcc g++ install zip nasm mono-complete make python
+apt-get -y -qq install git cmake gcc g++ install zip nasm mono-complete make python
 
 # Install the cmake platform template
 CMAKE_VERSION="$(cmake --version)";
