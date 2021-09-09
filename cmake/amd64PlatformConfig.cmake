a871150e53a520a1562a0ab65de40dcde41ac5bf
Philip Meulengracht <the_meulengracht@hotmail.com>
2021-09-09 14:54:09 +0200

    Initial changes to switch to toolchain 14

--- cmake/amd64PlatformConfig.cmake
+++ cmake/amd64PlatformConfig.cmake
@@ -4,22 +4,20 @@ if (NOT DEFINED VALI_BUILD)
     message (FATAL_ERROR "You must invoke the root cmake file, not the individual platform files")
 endif ()
 
-set (ARCH_FLAGS "-m64 -Damd64 -D__x86_64__ -D__STDC_FORMAT_MACROS_64 --target=amd64-pc-win32-itanium-coff -fdwarf-exceptions")
+set (ARCH_FLAGS "-m64 --target=amd64-uml-vali")
 set (WARNINGS_FLAGS "-Wno-address-of-packed-member -Wno-self-assign -Wno-unused-function")
-set (SHARED_FLAGS "-U_WIN32 -fms-extensions -Wall -nostdlib -nostdinc -O3 -DMOLLENOS")
-set (FEATURE_FLAGS "${FEATURE_FLAGS} \
-    -D__OSCONFIG_HAS_MMIO \
-    -D__OSCONFIG_ACPI_SUPPORT \
-    -D__OSCONFIG_HAS_UART \
-    -D__OSCONFIG_HAS_DWCAS"
-) # Assume presence of CPUID_FEAT_ECX_CX16 in cpuid
+set (SHARED_FLAGS "-fms-extensions -Wall -ffreestanding -nostdlib -nostdinc -O3")
 
-set (ASM_FLAGS "-f win64 -Xvc")
-set (VALI_TARGET_TRIPPLE "amd64-pc-win32-itanium-coff")
-
+set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_HAS_MMIO")
+set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_ACPI_SUPPORT")
+set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_HAS_UART")
+set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_HAS_DWCAS") # Assume presence of CPUID_FEAT_ECX_CX16 in cpuid
 if (NOT VALI_HEADLESS)
     set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_HAS_VIDEO")
 endif ()
+
+set (ASM_FLAGS "-f win64 -Xvc")
+set (VALI_TARGET_TRIPPLE "amd64-uml-vali")
 
 set (CMAKE_ASM_NASM_OBJECT_FORMAT win64)
 set (NASM_DEFAULT_FORMAT win64)
@@ -27,8 +25,8 @@ set (CMAKE_ASM_NASM_COMPILE_OBJECT "<CMAKE_ASM_NASM_COMPILER> <INCLUDES> \
     <FLAGS> -f ${CMAKE_ASM_NASM_OBJECT_FORMAT} -o <OBJECT> <SOURCE>")
 set (CMAKE_ASM_NASM_FLAGS "-Xvc -D${VALI_ARCH} -D__${VALI_ARCH}__ ${FEATURE_FLAGS}")
 
-set (CMAKE_C_FLAGS "-ffreestanding ${SHARED_FLAGS} ${ARCH_FLAGS} ${WARNINGS_FLAGS} ${FEATURE_FLAGS}")
-set (CMAKE_CXX_FLAGS "-std=c++17 -ffreestanding ${SHARED_FLAGS} ${ARCH_FLAGS} ${WARNINGS_FLAGS} ${FEATURE_FLAGS}")
+set (CMAKE_C_FLAGS "${ARCH_FLAGS} ${SHARED_FLAGS} ${WARNINGS_FLAGS} ${FEATURE_FLAGS}")
+set (CMAKE_CXX_FLAGS "${ARCH_FLAGS} -std=c++17 ${SHARED_FLAGS} ${WARNINGS_FLAGS} ${FEATURE_FLAGS}")
 
 set (VALI_COMPILER_RT_ASM_FLAGS ${CMAKE_C_FLAGS})
 set (VALI_COMPILER_RT_C_FLAGS "${CMAKE_C_FLAGS} -I${CMAKE_CURRENT_LIST_DIR}/../librt/include -I${CMAKE_CURRENT_LIST_DIR}/../librt/libc/include")
@@ -38,7 +36,7 @@ set (VALI_COMPILER_RT_TARGET clang_rt.builtins-x86_64)
 #set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} some other flags")
 #set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
 
-set (CMAKE_SHARED_LINKER_FLAGS "/nodefaultlib /machine:X64 /subsystem:native")
-set (CMAKE_EXE_LINKER_FLAGS "/nodefaultlib /machine:X64 /subsystem:native")
+set (CMAKE_SHARED_LINKER_FLAGS "/lldvpe")
+set (CMAKE_EXE_LINKER_FLAGS "/lldvpe")
 
 #if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
--- cmake/i386PlatformConfig.cmake
+++ cmake/i386PlatformConfig.cmake
@@ -4,20 +4,20 @@ if (NOT DEFINED VALI_BUILD)
     message (FATAL_ERROR "You must invoke the root cmake file, not the individual platform files")
 endif ()
 
-set (ARCH_FLAGS "-march=i686 -m32 -Di386 -D__i386__ --target=i386-pc-win32-itanium-coff")
+set (ARCH_FLAGS "-march=i686 -m32 --target=i386-uml-vali")
 set (WARNINGS_FLAGS "-Wno-address-of-packed-member -Wno-self-assign -Wno-unused-function")
-set (SHARED_FLAGS "-U_WIN32 -fms-extensions -Wall -nostdlib -nostdinc -O3 -DMOLLENOS")
-set (FEATURE_FLAGS "${FEATURE_FLAGS} \
-    -D__OSCONFIG_HAS_MMIO \
-    -D__OSCONFIG_ACPI_SUPPORT \
-    -D__OSCONFIG_HAS_UART \
-    -D__OSCONFIG_HAS_DWCAS") # Assume presence of CPUID_FEAT_EDX_CX8 in cpuid
-set (ASM_FLAGS "-f win32 -Xvc")
-set (VALI_TARGET_TRIPPLE "i386-pc-win32-itanium-coff")
+set (SHARED_FLAGS "-fms-extensions -Wall -ffreestanding -nostdlib -nostdinc -O3")
 
+set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_HAS_MMIO")
+set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_ACPI_SUPPORT")
+set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_HAS_UART")
+set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_HAS_DWCAS")# Assume presence of CPUID_FEAT_EDX_CX8 in cpuid
 if (NOT VALI_HEADLESS)
     set (FEATURE_FLAGS "${FEATURE_FLAGS} -D__OSCONFIG_HAS_VIDEO")
 endif ()
+
+set (ASM_FLAGS "-f win32 -Xvc")
+set (VALI_TARGET_TRIPPLE "i386-uml-vali")
 
 set (CMAKE_ASM_NASM_OBJECT_FORMAT win32)
 set (NASM_DEFAULT_FORMAT win32)
@@ -25,8 +25,8 @@ set (CMAKE_ASM_NASM_COMPILE_OBJECT "<CMAKE_ASM_NASM_COMPILER> <INCLUDES> \
     <FLAGS> -f ${CMAKE_ASM_NASM_OBJECT_FORMAT} -o <OBJECT> <SOURCE>")
 set (CMAKE_ASM_NASM_FLAGS "-Xvc -D${VALI_ARCH} -D__${VALI_ARCH}__ ${FEATURE_FLAGS}")
 
-set (CMAKE_C_FLAGS "-ffreestanding ${SHARED_FLAGS} ${ARCH_FLAGS} ${WARNINGS_FLAGS} ${FEATURE_FLAGS}")
-set (CMAKE_CXX_FLAGS "-std=c++17 -ffreestanding ${SHARED_FLAGS} ${ARCH_FLAGS} ${WARNINGS_FLAGS} ${FEATURE_FLAGS}")
+set (CMAKE_C_FLAGS "${ARCH_FLAGS} ${SHARED_FLAGS} ${WARNINGS_FLAGS} ${FEATURE_FLAGS}")
+set (CMAKE_CXX_FLAGS "${ARCH_FLAGS} -std=c++17 ${SHARED_FLAGS} ${WARNINGS_FLAGS} ${FEATURE_FLAGS}")
 
 set (VALI_COMPILER_RT_ASM_FLAGS ${CMAKE_C_FLAGS})
 set (VALI_COMPILER_RT_C_FLAGS "${CMAKE_C_FLAGS} -I${CMAKE_CURRENT_LIST_DIR}/../librt/include -I${CMAKE_CURRENT_LIST_DIR}/../librt/libc/include")
@@ -36,7 +36,7 @@ set (VALI_COMPILER_RT_TARGET clang_rt.builtins-i386)
 #set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} some other flags")
 #set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
 
-set (CMAKE_SHARED_LINKER_FLAGS "/nodefaultlib /machine:X86 /subsystem:native")
-set (CMAKE_EXE_LINKER_FLAGS "/nodefaultlib /machine:X86 /subsystem:native")
+set (CMAKE_SHARED_LINKER_FLAGS "/lldvpe")
+set (CMAKE_EXE_LINKER_FLAGS "/lldvpe")
 
 #if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
