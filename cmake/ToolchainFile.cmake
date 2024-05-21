fc665c0264e2582742bb6a65d12745604f680253
Philip Meulengracht <the_meulengracht@hotmail.com>
2024-05-21 11:44:40 +0200

    many: build improvements again

--- cmake/ToolchainFile.cmake
+++ cmake/ToolchainFile.cmake
@@ -1,11 +1,11 @@
 # Sanitize expected environmental variables
 set (CCROOT)
 set (CCROOT_FOUND FALSE)
-if(DEFINED ENV{CROSS})
-    set (CCROOT "$ENV{CROSS}")
-    set (CCROOT_FOUND TRUE)
-elseif(DEFINED ENV{VALICC})
+if(DEFINED ENV{VALICC})
     set (CCROOT "$ENV{VALICC}")
+    set (CCROOT_FOUND TRUE)
+elseif(DEFINED ENV{CROSS})
+    set (CCROOT "$ENV{CROSS}")
     set (CCROOT_FOUND TRUE)
 endif()
 
